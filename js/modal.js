748a3e14d539432654692513f0a557abf0598bed
ancorframe <yura.pundiak@gmail.com>
2022-06-09 13:58:11 +0300

    +

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,17 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-    body: document.querySelector("body")
+    openMenuBtn: document.querySelector("[data-menu-open]"),
+    closeMenuBtn: document.querySelector("[data-menu-close]"),
+    closeAnchorLink: document.querySelector("[data-anchor-close]"),
+    modal: document.querySelector("[data-menu]"),
+    body: document.querySelector("body"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
+  refs.openMenuBtn.addEventListener("click", toggleModal);
+  refs.closeMenuBtn.addEventListener("click", toggleModal);
+  refs.closeAnchorLink.addEventListener("click", toggleModal);
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
-    refs.body.classList.toggle("no-scroll")
+    refs.body.classList.toggle("no-scroll");
   }
 })();
